71dafb16640d08e495b1a16114a5ed6345bb8e1a
Pear Computer LLC. <the-new-devil@proton.me>
2025-04-28 19:40:00 -0400

    Update senyai.php to remove time limit of execution

--- senyai.php
+++ senyai.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('max_execution_time', 0);
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, command, method');
